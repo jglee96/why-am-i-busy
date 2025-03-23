0eb4dc59097f559e42c168200065cf3038fe2ac5
jglee96 <zakklee96@gmail.com>
2025-03-23 23:07:20 +0900

    fix: 인증 후 리디렉션 경로 수정 및 로드 밸런서 환경 처리 추가

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,16 +1,28 @@
-import { createClient } from "@/utils/supabase/server"
-import { type NextRequest, NextResponse } from "next/server"
+import { createClient } from "@/utils/supabase/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get("code")
+  const { origin, searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  const next = "/tracker";
 
   if (code) {
-    const supabase = createClient()
-    await supabase.auth.exchangeCodeForSession(code)
+    const supabase = createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+
+    const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+    const isLocalEnv = process.env.NODE_ENV === "development";
+    if (isLocalEnv) {
+      // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+      return NextResponse.redirect(`${origin}${next}`);
+    } else if (forwardedHost) {
+      return NextResponse.redirect(`https://${forwardedHost}${next}`);
+    } else {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
   }
 
   // 인증 후 트래커 페이지로 리디렉션
-  return NextResponse.redirect(new URL("/tracker", request.url))
+  return NextResponse.redirect(new URL("/404", request.url));
 }
-
